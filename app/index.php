8a194f3cdf96968cf02c9c7f2363476eae9acd9c
sakumashunsuke <s-sakuma@oz-vision.co.jp>
2015-07-11 03:20:37 +0900

    余計な空白を削除

--- app/index.php
+++ app/index.php
@@ -2,7 +2,7 @@
 require('util.php');
 
 // アプリケーション設定
-define('CONSUMER_KEY', ' hxlPpJHDZQU28NvsFVu0WMWcf');
+define('CONSUMER_KEY', 'hxlPpJHDZQU28NvsFVu0WMWcf');
 define('CONSUMER_SECRET', 'ho1vt4Z9YTY8qUxR7Nt7hCJNQucIkYH03c8NXmwPq0MTpm1fTB');
 define('CALLBACK_URL', 'http://192.168.33.19/callback.php');
 
